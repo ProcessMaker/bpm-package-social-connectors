0e322549140a475bd9b1ea33246a50ad70f91895
David Callizaya <davidcallizaya@gmail.com>
2018-11-26 17:53:54 -0400

    Update register extension.

--- resources/js/connectors/twitter/send/index.js
+++ resources/js/connectors/twitter/send/index.js
@@ -1,69 +1,89 @@
-import BpmnModdle from 'bpmn-moddle';
+import {
+  task,
+} from '@/components/nodes';
 
-let moddle = new BpmnModdle();
+window.ProcessMaker.EventBus.$on('modeler-init', ({ registerNode }) => {
+  /* Add a custom node example */
 
-import component from './component.vue';
-
-export default {
-  id: 'processmaker-connectors-social-twitter-send',
-  component: component,
-  bpmnType: 'bpmn:ScriptTask',
-  control: true,
-  category: 'Social',
-  icon: require('../icon.png'),
-  label: 'Send Tweet',
-  definition: function() {
-    return moddle.create('bpmn:ScriptTask', {
-      name: 'Send Tweet',
-      implementation: 'processmaker-social-twitter-send'
-    });
-  },
-  diagram: function() {
-    return moddle.create('bpmndi:BPMNShape', {
-      bounds: moddle.create('dc:Bounds', {
-        height: 80,
-        width: 100,
-      }),
-    });
-  },
-  inspectorHandler: function(value, definition, component) {
-    // Go through each property and rebind it to our data
-    for (var key in value) {
-      // Only change if the value is different
-      if (definition[key] != value[key]) {
-        definition[key] = value[key];
-      }
-    }
-    component.updateShape();
-  },
-  inspectorConfig: [
-    {
-      name: 'Send Tweet',
-      items: [
-        {
-          component: 'FormText',
-          config: {
-            label: 'Send Tweet',
-            fontSize: '2em',
-          },
-        },
-        {
-          component: 'FormInput',
-          config: {
-            label: 'Identifier',
-            helper: 'The id field should be unique across all elements in the diagram',
-            name: 'id',
-          },
-        },
-        {
-          component: 'FormTextArea',
-          config: {
-            label: 'Tweet Body',
-            helper: 'The Body Of The Tweet to Send',
-            name: 'tweet',
-          },
-        },
-      ],
+  const component = {
+    extends: task.component,
+    methods: {
+      handleClick() {
+        this.$parent.loadInspector('processmaker-connectors-social-twitter-send', this.node.definition, this);
+      },
     },
-  ],
-};
+  };
+
+  const implementation = 'processmaker-social-twitter-send';
+  const nodeId = 'processmaker-connectors-social-twitter-send';
+
+  const nodeType = {
+    id: nodeId,
+    component: component,
+    bpmnType: 'bpmn:ServiceTask',
+    control: true,
+    category: 'Social',
+    icon: require('@/assets/toolpanel/task.svg'),
+    label: 'Send Tweet',
+    definition: function (moddle) {
+      return moddle.create('bpmn:ServiceTask', {
+        name: 'Send Tweet',
+        implementation,
+      });
+    },
+    diagram: function (moddle) {
+      return moddle.create('bpmndi:BPMNShape', {
+        bounds: moddle.create('dc:Bounds', {
+          height: 80,
+          width: 100,
+        }),
+      });
+    },
+    inspectorHandler: function (value, definition, component) {
+      // Go through each property and rebind it to our data
+      for (var key in value) {
+        // Only change if the value is different
+        if (definition[key] != value[key]) {
+          definition[key] = value[key];
+        }
+      }
+      component.updateShape();
+    },
+    inspectorConfig: [
+      {
+        name: 'Send Tweet',
+        items: [
+          {
+            component: 'FormText',
+            config: {
+              label: 'Send Tweet',
+              fontSize: '2em',
+            },
+          },
+          {
+            component: 'FormInput',
+            config: {
+              label: 'Identifier',
+              helper: 'The id field should be unique across all elements in the diagram',
+              name: 'id',
+            },
+          },
+          {
+            component: 'FormTextArea',
+            config: {
+              label: 'Tweet Body',
+              helper: 'The Body Of The Tweet to Send',
+              name: 'tweet',
+            },
+          },
+        ],
+      },
+    ],
+  };
+
+  registerNode(nodeType, definition => {
+    if (definition.implementation === implementation) {
+      return nodeId;
+    }
+  });
+});
